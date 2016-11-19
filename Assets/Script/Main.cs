91ac7996e89c38a725e511ed3f978153e27e2730
absolsonga <absolsonga@naver.com>
2016-11-19 16:27:41 +0900

    add Scene #1

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+    public static int score = 0;
+    public static int highscore = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
